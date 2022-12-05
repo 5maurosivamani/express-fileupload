d7244b268fa0cc3b15bdbe5077b3a4646788eb00
Sivamani <5maurosivamani@gmail.com>
2022-12-05 07:33:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
+require("dotenv/config");
+
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("index.html");
+});
+
+app.post("/", (req, res) => {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.image;
+  uploadPath = __dirname + "/uploads/" + sampleFile.name;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+
+    res.send("File uploaded!");
+  });
+});
+
+app.listen(PORT, (err) => {
+  if (!err) {
+    console.log(`Server started at PORT: ${PORT}`);
+  }
+});
